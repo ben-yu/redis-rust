e44a720038c0a58e457717b65ec2d22cb45ced9d
Yu <me@ben-yu.com>
2025-10-02 17:22:19 -0700

    write PONG back

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused_imports)]
 use std::net::TcpListener;
+use std::io::Write;
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -10,7 +11,8 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
+            Ok(mut stream) => {
+                stream.write_all(b"+PONG\r\n").unwrap();
                 println!("accepted new connection");
             }
             Err(e) => {
