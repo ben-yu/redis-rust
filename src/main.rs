57bd1eba05e507b36e293e712a04208a92d284c0
Yu <me@ben-yu.com>
2025-10-02 21:47:00 -0700

    count PINGs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
-use std::io::Write;
+use std::{
+    io::{BufReader, Write, prelude::*},
+    net::{TcpListener, TcpStream},
+};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,13 +13,34 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                stream.write_all(b"+PONG\r\n").unwrap();
+            Ok(stream) => {
                 println!("accepted new connection");
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    // Count PING commands in the request
+    let ping_count = request.iter()
+        .filter(|line| *line == "*1\r\n$4\r\nPING\r\n")
+        .count();
+
+    // Send the same number of PONGs back
+    for _ in 0..ping_count {
+        stream.write_all(b"+PONG\r\n").unwrap();
+    }
+
+    println!("Request: {:#?}", request);
 }
